00535c107b9e10ed412221e3ed01fd6ca804d1e5
Vibhav Sharma <vibhav2000@vibhav.sharma2000@gmail.com>
2020-08-25 18:34:38 +0530

    first commit for sonar

---
+++ CICD.lab.sonar/src/main/java/test1.java
@@ -0,0 +1,22 @@
+
+public class test1 {
+	private int n=10;
+			public void f1(int n1)
+			{
+		n1=10;
+			}
+
+	public static void main(String[] args) {
+		int n2=20;
+		System.out.println("Hello Sonar");
+		test1 ob = new test1();
+		ob.f1(20);
+		if(n2==10)
+	
+		{
+			System.out.println("This is false part");
+			
+		}
+	}
+
+}
